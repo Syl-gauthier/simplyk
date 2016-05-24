b19d3069ad901bbab0eb22437ed4b0f056bf60fd
ThibautJ <thibaut.jaurou@gmail.com>
2016-05-24 14:52:31 -0400

    Models updated

--- models/user_model.js
+++ models/user_model.js
@@ -2,9 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
-var User = mongoose.model('User', new Schema({
-	name: String,
-	email: String
+var User = mongoose.model('Users', new Schema({
+	id: ObjectId,
+	fname: String,
+	lname: String,
+	email: String,
+	birth: Date
 }));
 
 module.exports = User;
