2cdc68fb9da8c9b2c3101767382a1024f18ada4d
ThibautJ <thibaut.jaurou@gmail.com>
2016-08-19 11:54:00 -0400

    Add profile page for organism

--- routes/o_profile.js
+++ routes/o_profile.js
@@ -7,7 +7,27 @@ var Volunteer = require('../models/volunteer_model.js');
 
 router.get('/organism/profile', permissions.requireGroup('organism'), function(req,res){
   console.log('Begin get /profile')
-  res.json(req.session.organism);
+  res.render('o_profile.jade', {organism: req.session.organism, session: req.session});
+});
+
+router.post('/volunteer/editPassword', function(req, res) {
+  //Contains current, new and confirm password
+  var passwords = req.body;
+
+  //Check for validity of password and coherence between new and confirm
+  if (!req.user.validPassword(passwords.current) || passwords.new != passwords.confirm) {
+    res.send({
+      success: false,
+      err: "blabla"
+    });
+  } else {
+    req.user.password = req.user.generateHash(passwords.new);
+    req.user.save(function(err) {
+      res.send({
+        success: true
+      });
+    });
+  }
 });
 
 module.exports = router;
