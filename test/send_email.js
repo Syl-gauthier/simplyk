3872ce55efdd2c9434849a99aa930dd57c743e1a
ThibautJ <thibaut.jaurou@gmail.com>
2016-09-02 09:56:39 -0400

    Test email_credentials

--- test/send_email.js
+++ test/send_email.js
@@ -1,8 +1,8 @@
 var emailer = require('../email/emailer.js')
 
 var content = {
-  recipient: 'arowana87@gmail.com',
-  name: 'Pierre',
+  recipient: 'thibaut.jaurou@gmail.com',
+  name: 'Thibaut',
   customMessage: 'Congratulation, create an event to get volunteers!'
 };
 
