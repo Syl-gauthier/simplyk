33fb49c6398565100a5e45294bf940777a3eaf81
ThibautJ <thibaut.jaurou@gmail.com>
2016-09-02 16:51:05 -0400

    Link to saucelabs

--- test/test.js
+++ test/test.js
@@ -2,10 +2,6 @@ var Mocha = require('mocha');
 var expect = require('chai').expect;
 var gmaps = require('../middlewares/gmaps.js');
 
-var mocha = new Mocha({
-    ui: "tdd",
-    reporter: "spec"
-});
 
 describe('Array', function(){
 	describe('#indexOf()', function(){
