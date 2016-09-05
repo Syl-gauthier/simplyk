3eb8f38f54932c0c6d02436a622f943ebee22b10
ThibautJ <thibaut.jaurou@gmail.com>
2016-09-05 15:27:08 -0400

    Add zuul

--- test/test.js
+++ test/test.js
@@ -1,22 +1,10 @@
 var expect = require('chai').expect;
-var gmaps = require('../middlewares/gmaps.js');
 
 
 describe('Array', function(){
 	describe('#indexOf()', function(){
 		it('should return -1 when the value is not present', function(){
-			expect([1,2,3].indexOf(4)).to.equal(-1);
+			expect([1,2,3].indexOf(4)).equal(-1);
 		})
 	});
 });
-
-describe('GMaps', function(){
-	describe('#codeAddress()', function(){
-		it('should return ZERO_RESULT if address wrong', function(){
-			const address = 'bloblo';
-			gmaps.codeAddress(address, function(lat, lon){
-				expect(lat).equal('ZERO_RESULTS');
-			});
-		})
-	})
-})
