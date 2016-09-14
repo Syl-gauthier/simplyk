d39d7087ceaa06ee24d0a81de191ea7093d56d32
ThibautJ <thibaut.jaurou@gmail.com>
2016-09-14 14:20:18 -0400

    .min js creation

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
-style-simplyk.less
+//REQUIRED
+
+var gulp = require('gulp'),
+	babel = require('gulp-babel'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify');
+
+//Scripts Task
+
+gulp.task('scripts', function() {
+	gulp.src(['public/javascripts/*.js', '!public/javascripts/*.min.js'])
+		//.pipe(babel())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/javascripts/'));
+});
+
+//Default Task
+
+gulp.task('default', ['scripts']);
