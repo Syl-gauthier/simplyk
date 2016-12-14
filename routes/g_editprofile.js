fc47496a094e43f355f4845b0d383fe1f9a9acfb
ThibautJ <thibaut.jaurou@gmail.com>
2016-12-14 08:54:44 -0500

    Add simplyk as a client school

--- routes/g_editprofile.js
+++ routes/g_editprofile.js
@@ -12,8 +12,8 @@ const Volunteer = require('../models/volunteer_model.js');
 const Organism = require('../models/organism_model.js');
 const Admin = require('../models/admin_model.js');
 
-const client_schools = ['École Père-Marquette', 'École Louis-Riel', 'École La Dauversière'];
-const client_schools_id = ['57e99d153d062714a2fa65e0', '581696f317afb6294243f786', '583f554990d8511b0bcfb0dd'];
+const client_schools = ['École Père-Marquette', 'École Louis-Riel', 'École La Dauversière', 'Simplyk'];
+const client_schools_id = ['57e99d153d062714a2fa65e0', '581696f317afb6294243f786', '583f554990d8511b0bcfb0dd', '584eee184ed1ec2ad30b947b'];
 
 router.post('*/edit-profile', permissions.requireGroup('volunteer', 'organism'), function(req, res) {
 	console.info('IN edit-form');
