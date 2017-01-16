27f822619891538978590b1ee954020f6b2aba27
ThibautJ <thibaut.jaurou@gmail.com>
2017-01-16 11:25:51 -0500

    Some french corrections

--- res/schools_res.js
+++ res/schools_res.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 module.exports = {
 	organism_questions: ['Quel point positif pouvez-vous mettre en avant sur l’élève, et qu’est-ce que l’élève pourrait améliorer ?'],
-	student_questions: ['Quel est le but du travail de l’organisme?', 'Identifie une qualité du profil de l’apprenant que tu as développé au cours de cette activité et explique pourquoi.', 'Comment pourrais-tu prolonger ton expérience de bénévolat ?']
+	student_questions: ['Quel est le but du travail de l’organisme?', 'Identifie une qualité du profil de l’apprenant que tu as développé au cours de cet engagement et explique pourquoi.', 'Comment pourrais-tu prolonger ton expérience de bénévolat ?']
 }
