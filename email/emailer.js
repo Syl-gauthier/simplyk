44c49170110dc6c60e4f29c67eaed4e79bcfadc1
ThibautJ <thibaut.jaurou@gmail.com>
2017-03-01 17:45:20 -0500

    Send email after event and longterm creation

--- email/emailer.js
+++ email/emailer.js
@@ -163,6 +163,56 @@ function sendUnsubscriptionEmail(content) {
   });
 };
 
+function sendTransAddEvent(content) {
+  content.subtitle = content.customMessage;
+  content.type = 'transaddevent';
+  content.title = 'Création d\'évènement';
+  content.button = {
+    text: 'Voir mon évènement',
+    link: 'www.simplyk.io/organism/dashboard'
+  };
+  verify_template.render(content, function(err, results) {
+    if (err) {
+      return console.error(err);
+    };
+
+    var mailOptions = {
+      from: '"François @ Simplyk" <contact@simplyk.io>', // sender address
+      to: content.recipient,
+      subject: content.event_name + ': évènement créé avec succès ', // Subject line
+      text: '', // plaintext body
+      html: results.html
+    };
+
+    callSendMail(mailOptions);
+  });
+};
+
+function sendTransAddLongTerm(content) {
+  content.subtitle = content.customMessage;
+  content.type = 'transaddlongterm';
+  content.title = 'Création d\'engagement';
+  content.button = {
+    text: 'Voir mon engagement',
+    link: 'www.simplyk.io/organism/dashboard'
+  };
+  verify_template.render(content, function(err, results) {
+    if (err) {
+      return console.error(err);
+    };
+
+    var mailOptions = {
+      from: '"François @ Simplyk" <contact@simplyk.io>', // sender address
+      to: content.recipient,
+      subject: content.longterm_name + ': engagement créé avec succès ', // Subject line
+      text: '', // plaintext body
+      html: results.html
+    };
+
+    callSendMail(mailOptions);
+  });
+};
+
 function sendHoursPendingOrgEmail(content) {
   content.subtitle = content.customMessage;
   content.type = 'hourspendingorg';
@@ -399,6 +449,8 @@ module.exports = {
   sendHoursPendingOrgEmail: sendHoursPendingOrgEmail,
   sendHoursConfirmedVolEmail: sendHoursConfirmedVolEmail,
   sendAutomaticSubscriptionOrgEmail,
+  sendTransAddEvent,
+  sendTransAddLongTerm,
   sendForgottenPasswordEmail,
   sendManualHoursEmail,
   sendOneDayReminderEmail,
