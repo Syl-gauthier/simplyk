f74a112f62804efdeaa90d2cd40090ba926a3f42
ThibautJ <thibaut.jaurou@gmail.com>
2017-03-14 16:58:15 -0400

    g_accueil and map are good

--- public/javascripts/initmap2.js
+++ public/javascripts/initmap2.js
@@ -164,7 +164,7 @@ function initMap() {
   const localization_bar_div = document.createElement('div');
   localization_bar_div.id = 'localization_bar_div';
   let loc_bar_content = [];
-  loc_bar_content.push('<div id="localization_bar_div" class="center-block" style="margin-right: 5px; margin-left: 5px;">');
+  loc_bar_content.push('<div id="localization_bar_div" class="center-block hidden-xs" style="margin-right: 5px; margin-left: 5px;">');
   loc_bar_content.push('<form class="form-inline" style="margin-top:10px;"><div class="input-group" style="min-width: 25%">');
   loc_bar_content.push('<input id="address_field" class="form-control" placeholder="Chercher une adresse">');
   loc_bar_content.push('<span id="loc_submit" class="btn btn-default input-group-addon"><i class="fa fa-search"></i></span>');
