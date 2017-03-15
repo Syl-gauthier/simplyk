27a764da1b4bd7562ce6cad1af0697837830fe6d
Thibaut Jaurou <ThibautJ@users.noreply.github.com>
2017-03-15 12:06:27 -0400

    webhook nexmo print req.body

--- routes/g_nav.js
+++ routes/g_nav.js
@@ -136,10 +136,18 @@ router.get('/robots.txt', function(req, res) {
 });
 
 router.get('/nexmo_inbound', function(req, res) {
+  console.info('In nexmo_inbound : ' + JSON.stringify(req.body));
+  console.info('In nexmo_inbound : ' + JSON.stringify(req.query));
   res.status(200).end();
 });
 
 router.post('/nexmo_inbound', function(req, res) {
+  console.info('In nexmo_inbound : ' + JSON.stringify(req.body));
+  res.status(200).end();
+});
+
+router.post('/nexmo_receipt', function(req, res) {
+  console.info('In nexmo_receipt : ' + JSON.stringify(req.body));
   res.status(200).end();
 });
 
